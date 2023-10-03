49069e59de4e47d688de45cd49074f2051bb11de
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-03 02:32:46 -0300

    ✨ Criação do botão de link para o footer ✨

---
+++ src/components/LinkButton/index.tsx
@@ -0,0 +1,33 @@
+import { IconProps } from '@phosphor-icons/react';
+import * as Tooltip from '@radix-ui/react-tooltip';
+import { ForwardRefExoticComponent, RefAttributes } from 'react';
+import styles from './styles.module.css';
+
+
+type LinkButtonProps = {
+  link: string;
+  label: string;
+  icon: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>;
+}
+
+
+export function LinkButton({ icon: Icon, label, link }: LinkButtonProps) {
+
+  return (
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <a href={link} target='_blank' className={styles.link} aria-label={label}>
+            <Icon size={26} weight='bold' />
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content sideOffset={5} className={styles.tooltipContent} aria-label=".">
+            {label}
+            <Tooltip.Arrow className={styles.tooltipArrow} />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  )
+}
