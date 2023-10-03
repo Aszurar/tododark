b3f9f33fc93747747a84aeb3f575d9e6302ec2ee
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-03 02:30:41 -0300

    ✨Criação do Checkbox ✨

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,41 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
+import styles from './styles.module.css';
+import { CheckIcon } from '../../assets/CheckIcon';
+import { TaskProps } from '../../dto/taskDTO';
+
+type CheckBoxProps = TaskProps & {
+  handleCheckTask: () => void;
+}
+
+export function CheckBox({ id, isChecked, title, handleCheckTask }: CheckBoxProps) {
+
+  const checkboxToggleStyle = isChecked ? styles.checkboxRootChecked :
+    styles.checkboxRootUnchecked;
+  const checkboxRootStyle = `${checkboxToggleStyle} ${styles.checkboxRoot}`;
+
+  const labelToggleStyle = isChecked ? styles.labelChecked :
+    styles.labelUnchecked;
+  const labelStyle = `${labelToggleStyle} ${styles.label}`;
+
+  return (
+    <div className={styles.container}>
+      <Checkbox.Root
+        id={id}
+        checked={isChecked}
+        className={checkboxRootStyle}
+        onCheckedChange={handleCheckTask}
+        aria-label="Marcar tarefa como concluída"
+      >
+        <Checkbox.Indicator className={styles.checkboxIndicator}>
+          <CheckIcon />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+
+      <label className={labelStyle} htmlFor={id}>
+        {title}
+      </label>
+
+    </div>
+
+  )
+}
