1aeee2ebd0c47f97644c2c4391376e3e48bffd1a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-03 02:34:41 -0300

    ✨ Criação do componente principal com a lsita de tasks e o input e botão de adicionar nova task✨

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,103 @@
+import { ChangeEvent, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+import { AddButton } from "../AddButton";
+import { Input } from "../Input";
+import { TaskCard } from "../TaskCard";
+
+import { MAX_TASK_TITLE_LENGTH, TASKS_LIST_DEFAULT, TaskProps } from "../../dto/taskDTO";
+
+import styles from './styles.module.css';
+
+export function Main() {
+  const [task, setTask] = useState("");
+  const [tasksList, setTasksList] = useState<TaskProps[]>(TASKS_LIST_DEFAULT);
+
+  const tasksListQuantity = tasksList.length;
+  const tasksListCheckedQuantity = tasksList.filter(task => task.isChecked).length;
+
+  function handleChangeTask(event: ChangeEvent<HTMLInputElement>) {
+    setTask(event.target.value);
+  }
+
+  function handleAddTask() {
+    event?.preventDefault();
+
+    if (task.trim() === '') {
+      return;
+    }
+
+    const isAlreadyTaskExists = tasksList.some(item => item.title === task);
+
+    if (isAlreadyTaskExists) {
+      return;
+    }
+
+    const taskFormatted =
+      task.length > MAX_TASK_TITLE_LENGTH
+        ? task.substring(0, MAX_TASK_TITLE_LENGTH)
+        : task;
+
+    const newTask: TaskProps = {
+      id: uuidv4(),
+      title: taskFormatted,
+      isChecked: false,
+    };
+
+    setTasksList(prevState => [...prevState, newTask]);
+    setTask('');
+  }
+
+  function handleCheckTask(id: string) {
+    const tasksListUpdated = tasksList.map(task => {
+      if (task.id === id) {
+        task.isChecked = !task.isChecked;
+      }
+      return task;
+    })
+
+    setTasksList(tasksListUpdated);
+  }
+
+  function handleDeleteTask(id: string) {
+    const newtasksListUpdated = tasksList.filter(task => task.id !== id);
+    setTasksList(newtasksListUpdated);
+  }
+
+  const tasksCardsList = tasksList.map((task) =>
+    <TaskCard
+      key={task.id}
+      id={task.id}
+      title={task.title}
+      isChecked={task.isChecked}
+      handleCheckTask={() => handleCheckTask(task.id)}
+      handleDeleteTask={() => handleDeleteTask(task.id)}
+    />
+
+  );
+
+  return (
+    <main className={styles.main}>
+      <header>
+        <form onSubmit={handleAddTask} >
+          <Input
+            task={task}
+            onChange={handleChangeTask}
+          />
+          <AddButton onClick={handleAddTask} />
+        </form>
+      </header>
+
+      <section className={styles.tasksList}>
+        <section className={styles.headerTasksList}>
+          <strong>Tarefas Criadas <span>{tasksListQuantity}</span></strong>
+          <strong>Concluídas <span>{tasksListCheckedQuantity} de {tasksListQuantity}</span></strong>
+        </section>
+
+        <section className={styles.tasksCardsList}>
+          {tasksCardsList}
+        </section>
+      </section>
+    </main>
+  )
+}
