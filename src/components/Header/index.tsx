ed74d8a06a72f777df901ae98e64a34a1f6bb268
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-03 02:32:09 -0300

    ✨ Criação do Header com a Logo ✨

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import Logo from '../../assets/Logo.png';
+import styles from './styles.module.css';
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt="Todo" />
+    </header>
+  )
+}
