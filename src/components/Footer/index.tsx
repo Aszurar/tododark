a2bd3e78b23ef0c0e386c739fbe2d575287ddecd
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-03 02:31:04 -0300

    ✨ Criação do footer com informações do desenvolvedor ✨

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import { GithubLogo, LinkedinLogo, ShootingStar } from "@phosphor-icons/react";
+
+import { LinkButton } from "../LinkButton";
+
+import styles from './styles.module.css'
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <strong>Desenvolvido por <ShootingStar size={20} weight='bold' /> <span>Lucas de Lima Martins de Souza.</span></strong>
+      <div className={styles.links}>
+        <LinkButton icon={GithubLogo} label="Acessar Github" link='https://github.com/Aszurar/igniteFeed' />
+        <LinkButton icon={LinkedinLogo} label="Acessar Linkedin" link='https://www.linkedin.com/in/lucas-de-lima-azsura/' />
+      </div>
+    </footer>
+  )
+}
