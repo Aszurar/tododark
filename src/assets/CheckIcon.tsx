998e21ae4539c19295f70f4686ec82ee9cc3e14c
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-03 02:29:45 -0300

    :bento: Adição da Logo e do ícone do checkbox :bento:

---
+++ src/assets/CheckIcon.tsx
@@ -0,0 +1,42 @@
+
+export function CheckIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="#fff"
+      viewBox="0 0 24 24"
+    >
+      <g
+        stroke="#fffff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4.8"
+      >
+        <g clipPath="url(#clip0_429_11249)">
+          <path stroke="#fff" strokeWidth="2.4" d="M20 7L10 17l-5-5"></path>
+        </g>
+        <defs>
+          <clipPath id="clip0_429_11249">
+            <path fill="#fff" d="M0 0H24V24H0z"></path>
+          </clipPath>
+        </defs>
+      </g>
+      <g>
+        <g clipPath="url(#clip0_429_11249)">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2.4"
+            d="M20 7L10 17l-5-5"
+          ></path>
+        </g>
+        <defs>
+          <clipPath>
+            <path fill="#fff" d="M0 0H24V24H0z"></path>
+          </clipPath>
+        </defs>
+      </g>
+    </svg>
+  );
+}
