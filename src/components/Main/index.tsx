105aba6d509cd45be876141e443d6bda9183c74e
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-08 23:36:43 -0300

    :fire: Remoção de código :fire:

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,4 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 import { AddButton } from "../AddButton";
 import { Input } from "../Input";
