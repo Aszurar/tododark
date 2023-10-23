ce0ea378488040d44a81dace3b417cc182e5a8a6
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-22 23:24:14 -0300

    :recycle: :iphone:✨ Reestrutura de toda lógica de marcar e remover tarefas evitando problemas com memo e adição das funcionalidades de marcar todas tarefas, desmarcar todas tarefas, excluir todas tarefas e inclusão de responsividade ✨:iphone: :recycle:

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,76 +1,113 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 
-import { AddButton } from "../AddButton";
-import { Input } from "../Input";
-import { TaskCard, TaskCardMemoized } from "../TaskCard";
+import { AddButton } from '../AddButton'
+import { Input } from '../Input'
+import { TaskCardMemoized } from '../TaskCard'
 
-import { MAX_TASK_TITLE_LENGTH, TASKS_LIST_DEFAULT, TaskProps } from "../../dto/taskDTO";
+import { MAX_TASK_TITLE_LENGTH, TaskProps } from '../../dto/taskDTO'
 
-import styles from './styles.module.css';
-import { taskCreate } from "../../storage/tasks/tasksCreate";
-import { tasksGetAll } from "../../storage/tasks/tasksGetAll";
-import { tasksDelete } from "../../storage/tasks/tasksDelete";
-import { taskCheck } from "../../storage/tasks/tasksCheck";
+import { taskCreate } from '../../storage/tasks/tasksCreate'
+import { tasksGetAll } from '../../storage/tasks/tasksGetAll'
+import { tasksDelete, tasksDeleteAll } from '../../storage/tasks/tasksDelete'
+import {
+  taskCheck,
+  checkAllTasks,
+  uncheckAllTasks,
+} from '../../storage/tasks/tasksCheck'
+import { ListChecks, Trash } from '@phosphor-icons/react'
+import { ButtonWithTooltip } from '../ButtonWithTooltip'
+
+import styles from './styles.module.css'
 
 export function Main() {
-  const [task, setTask] = useState("");
-  const [tasksList, setTasksList] = useState<TaskProps[]>([]);
+  const [task, setTask] = useState('')
+  const [tasksList, setTasksList] = useState<TaskProps[]>([])
+  const [isCheckAllTasks, setIsCheckAllTasks] = useState(false)
 
-  const tasksListQuantity = tasksList.length;
-  const tasksListCheckedQuantity = tasksList.filter(task => task.isChecked).length;
+  const taskCharactersQuantity = task.length
+  const tasksListQuantity = tasksList.length
+  const tasksListCheckedQuantity = tasksList.filter(
+    (task) => task.isChecked,
+  ).length
+  const isDisabledCheckAllButtons = tasksListQuantity === 0
 
-  function handleChangeTask(event: ChangeEvent<HTMLInputElement>) {
-    setTask(event.target.value);
+  function handleChangeTask(event: ChangeEvent<HTMLTextAreaElement>) {
+    setTask(event.target.value)
+  }
+
+  function onSortByDateDescending(list: TaskProps[]) {
+    return list.sort((a, b) => {
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    })
+  }
+
+  function onSortByChecked(list: TaskProps[]) {
+    return list.sort((a, b) => {
+      return Number(b.isChecked) - Number(a.isChecked)
+    })
   }
 
   function updateTasksList() {
-    const tasksListUpdated = tasksGetAll();
-    setTasksList(tasksListUpdated);
+    const tasksListUpdated = tasksGetAll()
+    const tasksListSortedByDateDescending =
+      onSortByDateDescending(tasksListUpdated)
+    const tasksListSortedByChecked = onSortByChecked(
+      tasksListSortedByDateDescending,
+    )
+
+    setTasksList(tasksListSortedByChecked)
   }
 
   function handleAddTask(event: FormEvent) {
-    event?.preventDefault();
+    event?.preventDefault()
 
     if (task.trim() === '') {
-      return;
+      return
     }
 
-    const isAlreadyTaskExists = tasksList.some(item => item.title === task);
-
+    const isAlreadyTaskExists = tasksList.some((item) => item.title === task)
     if (isAlreadyTaskExists) {
-      return;
+      return
     }
 
     const taskFormatted =
       task.length > MAX_TASK_TITLE_LENGTH
         ? task.substring(0, MAX_TASK_TITLE_LENGTH)
-        : task;
+        : task
 
     const newTask: TaskProps = {
       id: crypto.randomUUID(),
       title: taskFormatted,
       isChecked: false,
-      createdAt: new Date()
-    };
+      createdAt: new Date(),
+    }
 
-    taskCreate(newTask);
-    updateTasksList();
-    setTask('');
-
+    taskCreate(newTask)
+    updateTasksList()
+    setTask('')
   }
 
   function handleCheckTask(id: string) {
     taskCheck({ id })
-    updateTasksList();
+    updateTasksList()
   }
 
   function handleDeleteTask(id: string) {
-    tasksDelete({ id });
-    updateTasksList();
+    tasksDelete({ id })
+    updateTasksList()
   }
 
-  const tasksCardsList = tasksList.map((task) =>
-    <TaskCard
+  function handleToggleCheckAllTasks() {
+    setIsCheckAllTasks((prevState) => !prevState)
+  }
+
+  function handleDeleteAllTasks() {
+    tasksDeleteAll()
+    updateTasksList()
+  }
+
+  const tasksCardsList = tasksList.map((task) => (
+    <TaskCardMemoized
       key={task.id}
       id={task.id}
       title={task.title}
@@ -79,38 +116,77 @@ export function Main() {
       handleCheckTask={() => handleCheckTask(task.id)}
       handleDeleteTask={() => handleDeleteTask(task.id)}
     />
-  );
+  ))
 
   useEffect(() => {
-    console.log(tasksList);
-    console.log("testeando:",)
-  }, [tasksList]);
-
-  useEffect(() => {
-    updateTasksList();
+    updateTasksList()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    if (isCheckAllTasks) {
+      checkAllTasks()
+    } else {
+      uncheckAllTasks()
+    }
+    updateTasksList()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isCheckAllTasks])
+
+  useEffect(() => {
+    if (tasksListQuantity === 0) {
+      setIsCheckAllTasks(false)
+    }
+  }, [tasksListQuantity])
 
   return (
     <main className={styles.main}>
       <header>
-        <form onSubmit={handleAddTask} >
-          <Input
-            task={task}
-            onChange={handleChangeTask}
-          />
+        <form onSubmit={handleAddTask}>
+          <Input task={task} onChange={handleChangeTask} />
           <AddButton />
         </form>
+        {taskCharactersQuantity > 0 && (
+          <span className={styles.taskCharactersQuantity}>
+            {taskCharactersQuantity} /{' '}
+            <span className={styles.taskCharacterLimit}>
+              {MAX_TASK_TITLE_LENGTH}
+            </span>
+          </span>
+        )}
       </header>
 
       <section className={styles.tasksList}>
         <section className={styles.headerTasksList}>
-          <strong>Tarefas Criadas <span>{tasksListQuantity}</span></strong>
-          <strong>Concluídas <span>{tasksListCheckedQuantity} de {tasksListQuantity}</span></strong>
+          <strong>
+            Tarefas Criadas <span>{tasksListQuantity}</span>
+          </strong>
+
+          <div className={styles.checkAllContainer}>
+            <ButtonWithTooltip
+              icon={ListChecks}
+              isActive={isCheckAllTasks}
+              onPress={handleToggleCheckAllTasks}
+              label="Marcar todas tarefas"
+              isDisabled={isDisabledCheckAllButtons}
+            />
+            <ButtonWithTooltip
+              icon={Trash}
+              isActive={isCheckAllTasks}
+              label="Excluir todas tarefas"
+              onPress={handleDeleteAllTasks}
+              isDisabled={!isCheckAllTasks}
+            />
+          </div>
+          <strong>
+            Concluídas{' '}
+            <span>
+              {tasksListCheckedQuantity} de {tasksListQuantity}
+            </span>
+          </strong>
         </section>
 
-        <section className={styles.tasksCardsList}>
-          {tasksCardsList}
-        </section>
+        <section className={styles.tasksCardsList}>{tasksCardsList}</section>
       </section>
     </main>
   )
