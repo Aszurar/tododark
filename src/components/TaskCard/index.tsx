0b50f86e848bdfa1fd29819d1a49c0952c0ecfaa
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-22 23:17:52 -0300

    :sparkles: Adição da funcionalidade de remover task no Card da task e registro do tempo da task de quando foi criada :sparkles:

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,12 +1,15 @@
-import { Trash } from '@phosphor-icons/react';
-import styles from './styles.module.css';
-import { CheckBox } from '../CheckBox';
-import { TaskProps } from '../../dto/taskDTO';
-import { memo } from 'react';
+import { Trash } from '@phosphor-icons/react'
+import styles from './styles.module.css'
+import { CheckBox } from '../CheckBox'
+import { TaskProps } from '../../dto/taskDTO'
+import { memo } from 'react'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { utcToZonedTime } from 'date-fns-tz'
 
 type TaskCardProps = TaskProps & {
-  handleCheckTask: () => void;
-  handleDeleteTask: () => void;
+  handleCheckTask: () => void
+  handleDeleteTask: () => void
 }
 
 export function TaskCard({
@@ -15,28 +18,60 @@ export function TaskCard({
   isChecked,
   createdAt,
   handleCheckTask,
-  handleDeleteTask
+  handleDeleteTask,
 }: TaskCardProps) {
+  const createdAtDate = new Date(createdAt)
+
+  const createdAtDateFormattedRelativeToNow = formatDistanceToNow(
+    createdAtDate,
+    {
+      locale: ptBR,
+      addSuffix: true,
+    },
+  )
+  const timeZone = 'America/Sao_Paulo'
+  const zonedDate = utcToZonedTime(createdAtDate, timeZone)
+  const formattedZonedDate = format(
+    zonedDate,
+    "dd 'de' MMMM 'de' yyyy, 'às' HH:mm:ss",
+    { locale: ptBR },
+  )
+  const formattedDateUTCISO = format(
+    createdAtDate,
+    "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+    { locale: ptBR },
+  )
 
   return (
     <article className={styles.card}>
-      <CheckBox
-        id={id}
-        isChecked={isChecked}
-        title={title}
-        handleCheckTask={handleCheckTask} />
+      <div className={styles.subContainer}>
+        <CheckBox
+          id={id}
+          isChecked={isChecked}
+          title={title}
+          handleCheckTask={handleCheckTask}
+        />
+        <button
+          type="button"
+          className={styles.deleteButton}
+          aria-label="Excluir tarefa"
+          onClick={handleDeleteTask}
+        >
+          <Trash size={20} weight="bold" />
+        </button>
+      </div>
 
-      <button
-        type='button'
-        className={styles.deleteButton}
-        aria-label="Excluir tarefa"
-        onClick={handleDeleteTask}>
-        <Trash size={20} weight='bold' />
-      </button>
+      <time
+        title={formattedZonedDate}
+        dateTime={formattedDateUTCISO}
+        className={styles.createdAt}
+      >
+        {createdAtDateFormattedRelativeToNow}
+      </time>
     </article>
-  );
+  )
 }
 
 export const TaskCardMemoized = memo(TaskCard, (prevProps, nextProps) => {
-  return prevProps.isChecked === nextProps.isChecked;
-});
+  return prevProps.isChecked === nextProps.isChecked
+})
