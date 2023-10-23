e7c56c60630ae231f07b797a29cdcd03bb3df027
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-22 23:22:48 -0300

    ✨Adição da função de remover todas tarefas ✨

--- src/storage/tasks/tasksDelete.ts
+++ src/storage/tasks/tasksDelete.ts
@@ -1,17 +1,17 @@
-import { TaskProps } from "../../dto/taskDTO";
-import { TASK_COLLECTION } from "../storageConfig";
-import { tasksGetAll } from "./tasksGetAll";
+import { TaskProps } from '../../dto/taskDTO'
+import { TASK_COLLECTION } from '../storageConfig'
+import { tasksGetAll } from './tasksGetAll'
 
 type TaskDeleteProps = Pick<TaskProps, 'id'>
 
 export function tasksDelete({ id }: TaskDeleteProps) {
-    try {
-        const storage = tasksGetAll();
-        const storageFiltered = storage.filter((task) => task.id !== id);
-        const storageFilteredFormatted = JSON.stringify(storageFiltered);
+  const storage = tasksGetAll()
+  const storageFiltered = storage.filter((task) => task.id !== id)
+  const storageFilteredFormatted = JSON.stringify(storageFiltered)
 
-        localStorage.setItem(TASK_COLLECTION, storageFilteredFormatted);
-    } catch (error) {
-        throw error;
-    }
+  localStorage.setItem(TASK_COLLECTION, storageFilteredFormatted)
+}
+
+export function tasksDeleteAll() {
+  localStorage.removeItem(TASK_COLLECTION)
 }
