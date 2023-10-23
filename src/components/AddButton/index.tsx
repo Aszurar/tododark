301d7dc82daa318d439cea79a2d3d301278b0d61
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-22 23:13:23 -0300

    ✨ Criação dos componentes de Botão de adicionar task e Input ✨

--- src/components/AddButton/index.tsx
+++ src/components/AddButton/index.tsx
@@ -1,12 +1,32 @@
-import styles from './styles.module.css';
-import { PlusCircle } from "@phosphor-icons/react";
+import { useEffect, useState } from 'react'
+import styles from './styles.module.css'
+import { PlusCircle } from '@phosphor-icons/react'
 
 export function AddButton() {
+  const [currentBrowserWidth, setCurrentBrowserWidth] = useState<number>(0)
+
+  const hasTitleButton = currentBrowserWidth < 480
+
+  function onUpdateCurrentBrowserWidth() {
+    setCurrentBrowserWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', onUpdateCurrentBrowserWidth)
+    return () => {
+      window.removeEventListener('resize', onUpdateCurrentBrowserWidth)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    console.log('Largura da tela', currentBrowserWidth)
+  }, [currentBrowserWidth])
+
   return (
-    <button
-      type="submit"
-      className={styles.button}>
-      Criar <PlusCircle size={16} weight='bold' />
+    <button type="submit" className={styles.button}>
+      {!hasTitleButton && 'Criar'}
+      <PlusCircle size={16} weight="bold" />
     </button>
   )
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,17 @@
-import { ChangeEvent } from 'react';
-import styles from './styles.module.css';
+import { ChangeEvent } from 'react'
+import styles from './styles.module.css'
+import { MAX_TASK_TITLE_LENGTH } from '../../dto/taskDTO'
 
 type InputProps = {
-  task: string;
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  task: string
+  onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 export function Input({ task, onChange }: InputProps) {
   return (
-    <input
-      type="text"
+    <textarea
       value={task}
+      maxLength={MAX_TASK_TITLE_LENGTH}
       onChange={onChange}
       className={styles.input}
       placeholder="Adicione uma nova tarefa"
