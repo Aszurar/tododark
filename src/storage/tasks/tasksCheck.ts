75cc61060eca92aef615f359d1f4d074330fbd6f
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-10-22 23:22:28 -0300

    ✨ dição da função de marcar todas tarefas e desmarcar todas tarefas ✨

--- src/storage/tasks/tasksCheck.ts
+++ src/storage/tasks/tasksCheck.ts
@@ -1,24 +1,40 @@
-import { TaskProps } from "../../dto/taskDTO";
-import { TASK_COLLECTION } from "../storageConfig";
-import { tasksGetAll } from "./tasksGetAll";
+import { TaskProps } from '../../dto/taskDTO'
+import { TASK_COLLECTION } from '../storageConfig'
+import { tasksGetAll } from './tasksGetAll'
 
 type TaskCheckProps = Pick<TaskProps, 'id'>
 
 export function taskCheck({ id }: TaskCheckProps) {
-    try {
-
-        const storage = tasksGetAll();
-        const storageUpdated = storage.map(task => {
-            if (task.id === id) {
-                task.isChecked = !task.isChecked;
-            }
-            return task;
-        })
-        const storageFormatted = JSON.stringify(storageUpdated);
-
-        localStorage.setItem(TASK_COLLECTION, storageFormatted);
-
-    } catch (error) {
-        throw error;
+  const storage = tasksGetAll()
+  const storageUpdated = storage.map((task) => {
+    if (task.id === id) {
+      task.isChecked = !task.isChecked
     }
+    return task
+  })
+  const storageFormatted = JSON.stringify(storageUpdated)
+
+  localStorage.setItem(TASK_COLLECTION, storageFormatted)
+}
+
+export function checkAllTasks() {
+  const storage = tasksGetAll()
+  const storageUpdated = storage.map((task) => {
+    task.isChecked = true
+    return task
+  })
+  const storageFormatted = JSON.stringify(storageUpdated)
+
+  localStorage.setItem(TASK_COLLECTION, storageFormatted)
+}
+
+export function uncheckAllTasks() {
+  const storage = tasksGetAll()
+  const storageUpdated = storage.map((task) => {
+    task.isChecked = false
+    return task
+  })
+  const storageFormatted = JSON.stringify(storageUpdated)
+
+  localStorage.setItem(TASK_COLLECTION, storageFormatted)
 }
