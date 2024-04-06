8c424fa1081de9e6cd922fcf6ac4364cb2240514
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-04-06 14:29:14 -0300

    ♿️ Adiciona um título para acessibilidade no cabeçalho ♿️

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={Logo} alt="Todo" loading="lazy" decoding="async" />
+      <h1 className={styles.sronly}>todo</h1>
     </header>
   )
 }
